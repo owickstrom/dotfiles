4b89702b38ae49496c46ebebbf93dd1ef10fdf16
Oskar Wickström <oskar.wickstrom@live.com>
2014-02-28 07:09:57 +0100

    Install vim-now for ruby support in vim.

--- ubuntu-scripts/install-packages.sh
+++ ubuntu-scripts/install-packages.sh
@@ -3,5 +3,5 @@
 # Ubuntu-only stuff. Abort if not Ubuntu.
 [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || exit 1
 
-DEPS="git tmux curl wget"
+DEPS="git tmux curl wget vim-nox"
 sudo apt-get install $DEPS
