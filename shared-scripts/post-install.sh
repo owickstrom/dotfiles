c6ae21680335869d6fda5a1b0226ed4f40db7238
Oskar Wickström <oskar.wickstrom@live.com>
2014-10-19 08:49:59 +0200

    Add nvm for both platforms

--- shared-scripts/post-install.sh
+++ shared-scripts/post-install.sh
@@ -5,6 +5,19 @@ source $DIR/../common.sh
 
 e_header "Post install"
 
+e_info "Installing nvm..."
+curl https://raw.githubusercontent.com/creationix/nvm/v0.17.2/install.sh | bash
+if [[ $? -eq 0 ]]; then
+    source ~/.nvm/nvm.sh
+    nvm install 0.10
+    nvm alias default 0.10
+    nvm use default
+    e_success "Installed nvm."
+else
+    e_warn "Failed to install nvm."
+fi
+
+e_info "Making vimproc..."
 make --directory="$DIR/../vim/bundle/vimproc.vim"
 if [[ $? -eq 0 ]]; then
     e_success "Make vimproc"
