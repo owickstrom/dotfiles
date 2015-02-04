ec7c981c269ffb6e90d00d174a123bb51419815d
Oskar Wickström <oskar.wickstrom@live.com>
2015-02-04 07:35:17 +0100

    Install golang and mercurial for ubuntu

--- linux-scripts/install-packages.sh
+++ linux-scripts/install-packages.sh
@@ -6,16 +6,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../common.sh
 
 if [ $(command -v apt-get) ]; then
-  DEPS="git tmux curl wget vim-nox irssi exuberant-ctags pandoc"
+  DEPS="git tmux curl wget vim-nox irssi exuberant-ctags pandoc golang mercurial"
   sudo apt-get install $DEPS
 fi
 
 if [ $(command -v yum) ]; then
+  e_warn "This installation will not have golang and mercurial installed. Please update dotfiles."
   DEPS="git tmux curl wget vim-enhanced irssi ctags pandoc"
   sudo yum install $DEPS
 fi
 
 if [ $(command -v pacman) ]; then
+  e_warn "This installation will not have golang and mercurial installed. Please update dotfiles."
   DEPS="git tmux curl wget vim irssi ctags base-devel pandoc"
   sudo pacman -S $DEPS
 fi
