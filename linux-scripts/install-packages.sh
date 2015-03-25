50fa471044efa3fd9269d5d0ef16916da19207b8
Oskar Wickström <oskar.wickstrom@live.com>
2015-03-25 08:31:10 +0100

    Themes and ghc stuff

--- linux-scripts/install-packages.sh
+++ linux-scripts/install-packages.sh
@@ -8,6 +8,9 @@ source $DIR/../common.sh
 if [ $(command -v apt-get) ]; then
   DEPS="git tmux curl wget vim-nox irssi exuberant-ctags pandoc golang mercurial"
   sudo apt-get install $DEPS
+
+  # Install Source Code Pro
+  curl -L https://gist.githubusercontent.com/lucasdavila/3875946/raw/f62c8a26ad6648b00cd379704617d0b61338f59f/install_source_code_pro.sh | bash
 fi
 
 if [ $(command -v yum) ]; then
