b74dd9fccae014a7cf0addfb21b5684374c42a69
Oskar Wickström <oskar.wickstrom@gmail.com>
2018-12-31 09:33:59 +0100

    Include VSCode config

--- src/Dotfiles/Symlink.hs
+++ src/Dotfiles/Symlink.hs
@@ -1,13 +1,13 @@
 module Dotfiles.Symlink (symlinkDotfiles, symlinkConfigFiles) where
 
-import System.FilePath
-import System.Posix.Files
-import System.Directory
-import Text.Printf
-import Control.Monad
-import Dotfiles.Core
-import Dotfiles.Print
-import Dotfiles.Process
+import           Control.Monad
+import           Dotfiles.Core
+import           Dotfiles.Print
+import           Dotfiles.Process
+import           System.Directory
+import           System.FilePath
+import           System.Posix.Files
+import           Text.Printf
 
 isSymbolicLinkTo :: FilePath -> FilePath -> IO Bool
 p1 `isSymbolicLinkTo` p2 =
@@ -17,63 +17,74 @@ p1 `isSymbolicLinkTo` p2 =
 symlinkDotfile :: Config -> FilePath -> IO ()
 symlinkDotfile (Config _ _ br) source = do
   home <- getHomeDirectory
-  let fileName = takeFileName source
+  let fileName     = takeFileName source
       backupTarget = br </> ("." ++ fileName)
-      target = home </> ("." ++ fileName)
-
+      target       = home </> ("." ++ fileName)
   symlinked <- target `isSymbolicLinkTo` source
-  if symlinked then
-    warning $ printf "%s already symlinked to dotfiles." target
-  else
-    do exists <- fileExist target
-       when exists $ do
-	 info $ "Renaming " ++ target ++ " to " ++ backupTarget
-	 createDirectoryIfMissing True br
-         rename target backupTarget
-         info $ printf "Created backup of %s in %s" target backupTarget
-       createSymbolicLink source target
-       success $ printf "Created symlink to %s" source
+  if symlinked
+    then warning $ printf "%s already symlinked to dotfiles." target
+    else do
+      exists <- fileExist target
+      when exists $ do
+        info $ "Renaming " ++ target ++ " to " ++ backupTarget
+        createDirectoryIfMissing True br
+        rename target backupTarget
+        info $ printf "Created backup of %s in %s" target backupTarget
+      createSymbolicLink source target
+      success $ printf "Created symlink to %s" source
 
-symlinkConfigFile :: Config -> FilePath -> IO ()
-symlinkConfigFile (Config _ _ br) source = do
+symlinkConfigFile :: Config -> FilePath -> FilePath -> IO ()
+symlinkConfigFile (Config _ _ br) configDir source = do
   home <- getHomeDirectory
-  let fileName = takeFileName source
-      backupTarget = br </> "config" </> fileName
-      target = home </> ".config" </> fileName
+  let relPath = makeRelative configDir source
+      backupTarget = br </> "config" </> relPath
+      target       = home </> ".config" </> relPath
   symlinked <- target `isSymbolicLinkTo` source
-  if symlinked then
-    warning $ printf "%s already symlinked to config directory." target
-  else
-    do exists <- fileExist target
-       when exists $ do info $ "Renaming " ++ target ++ " to " ++ backupTarget
-                        createDirectoryIfMissing True (br </> "config")
-                        rename target backupTarget
-                        info $ printf "Created backup of %s in %s" target backupTarget
-       createSymbolicLink source target
-       success $ printf "Created symlink to %s" source
+  if symlinked
+    then warning $ printf "%s already symlinked to config directory." target
+    else do
+      exists <- fileExist target
+      when exists $ do
+        info $ "Renaming " ++ target ++ " to " ++ backupTarget
+        createDirectoryIfMissing True (takeDirectory backupTarget)
+        rename target backupTarget
+        info $ printf "Created backup of %s in %s" target backupTarget
+      createDirectoryIfMissing True (takeDirectory target)
+      createSymbolicLink source target
+      success $ printf "Created symlink to %s" source
 
-validDotfilePath :: FilePath -> Bool
-validDotfilePath ".." = False
-validDotfilePath "." = False
-validDotfilePath _ = True
+getFilesAndDirsIn :: FilePath -> IO [FilePath]
+getFilesAndDirsIn dir = fmap (dir </>) <$> listDirectory dir
 
-getFilesInDirectory :: FilePath -> IO [FilePath]
-getFilesInDirectory dir = do
-  names <- getDirectoryContents dir
-  return $ map (dir </>) $ filter validDotfilePath names
+getFilesRecursiveIn :: FilePath -> IO [FilePath]
+getFilesRecursiveIn dir = foldMap (onPath dir) =<< listDirectory dir
+  where
+    onPath baseDir p = do
+      let absPath = baseDir </> p
+      isFile' <- doesFileExist absPath
+      if isFile'
+        then return [absPath]
+        else getFilesRecursiveIn absPath
 
 symlinkDotfiles :: Config -> IO ()
 symlinkDotfiles config@(Config os' r _) = do
   let specificDirName = case os' of
                       (Linux _) -> "linux-dotfiles"
-                      OSX -> "osx-dotfiles"
+                      OSX       -> "osx-dotfiles"
       special = [r </> "vim"]
 
-  specific <- getFilesInDirectory (r </> specificDirName)
-  shared <- getFilesInDirectory (r </> "shared-dotfiles")
+  specific <- getFilesAndDirsIn (r </> specificDirName)
+  shared <- getFilesAndDirsIn (r </> "shared-dotfiles")
   mapM_ (symlinkDotfile config) (special ++ specific ++ shared)
 
 symlinkConfigFiles :: Config -> IO ()
-symlinkConfigFiles config@(Config os' r _) =
-  mapM_ (symlinkConfigFile config) =<< getFilesInDirectory (r </> "config")
-
+symlinkConfigFiles config@(Config os' r _) = do
+  let configDir       = r </> "config"
+      specialDirNames = ["nvim", "Code" </> "User" </> "snippets"]
+      specialDirs     = (configDir </>) <$> specialDirNames
+  dirs    <- listDirectory configDir
+  -- we don't want to recursively go into the nvim dir with all it's
+  -- git repos, so it's special-cased here
+  regular <- foldMap getFilesRecursiveIn
+                     ((configDir </>) <$> filter (/= "nvim") dirs)
+  mapM_ (symlinkConfigFile config configDir) (specialDirs <> regular)
